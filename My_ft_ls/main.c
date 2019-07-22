1b5de7f9ea989a1c5c32243211263c275eefaa8d
Damian ODENDAAL <dodendaa@c4r17s9.wethinkcode.co.za>
2019-07-22 14:15:40 +0200

    fixes made

--- My_ft_ls/main.c
+++ My_ft_ls/main.c
@@ -6,21 +6,56 @@
 /*   By: dodendaa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 10:19:04 by dodendaa          #+#    #+#             */
-/*   Updated: 2019/07/22 10:24:03 by dodendaa         ###   ########.fr       */
+/*   Updated: 2019/07/22 13:30:48 by dodendaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
+typedef	struct s_ls_flags
+	{
+		int f_a;
+		int f_l;
+		int f_r;
+		int f_R;
+		int f_t;
+	} t_ls_flags;
+
+void	init_flags(t_ls_flags *flags)
+{
+	flags->f_R = 0;
+	flags->f_r = 0;
+	flags->f_t = 0;
+	flags->f_l = 0;
+	flags->f_a = 0;
+
+}
+
+void	check_flags(char *s, t_ls_flags *flg)
+{
+	char *s2;
+	int i;
+	int i2;
+
+	i = 0;// we start at 0 because we arent gonna cater for the - when we do the ls
+	i2 = 0;
+	s2 = "Rlart";
+	while(s[++i])
+	{
+		while(s2[i2])
+		{
+			if (s[i] == s2[i2])
+			{
+
+			}
+			i2++;
+		}
+	}
+}
+
 int main (int argc, char **argv)
 {
 	
-	if (argc < 2)
-	{
-		if (argc == 1)
-		{
-			
-		}
-	}
-	return (0);
+
+
 }
